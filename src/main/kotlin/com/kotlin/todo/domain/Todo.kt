ebfe381c9c552452c635ec6b529de1370e8da3e8
최준우 <junwoochoi@zuminternet.com>
2019-12-06 16:58:02 +0900

    feat : add Todo Entity

---
+++ src/main/kotlin/com/kotlin/todo/domain/Todo.kt
@@ -0,0 +1,21 @@
+package com.kotlin.todo.domain
+
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0
+    @Lob
+    @Column(name = "content")
+    var content: String? = null
+    @Column(name = "done")
+    var done: Boolean = false
+    @Column(name = "created_at")
+    var createdAt: LocalDateTime = LocalDateTime.now()
+    @Column(name = "modifed_at")
+    var modifedAt: LocalDateTime = createdAt
+
+}
